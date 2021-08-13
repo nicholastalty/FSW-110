a4f130062c7ec4c9c7910159175d0ab688a117c0
nicholastalty <nicholas.talty@bryanuniversity.edu>
2021-08-13 12:13:57 -0700

    put week 1 into github

---
+++ Week1/PnPSelectingElements/Elements.js
@@ -0,0 +1,8 @@
+//selecting a single element from the page => returns us an object
+
+//selecting multiple elements => returns us an array of elements
+
+
+var greeting = document.getElementbyId('hello')
+console.log(greeting)
+console.dir(greeting)
