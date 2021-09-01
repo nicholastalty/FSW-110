303171efcfe636cfd2882167ccad1458c01df1aa
nicholastalty <nicholas.talty@bryanuniversity.edu>
2021-09-01 14:11:50 -0700

    added PnP ChangingClasses assignment

--- Week3/PnPChangingClassesandStylesUsingJS/index.js
+++ Week3/PnPChangingClassesandStylesUsingJS/index.js
@@ -1,20 +1,11 @@
-// var person ={name: 'patrick'}
-// person.name = 'morgan'
-// console.log(person)
-
-
-// var heading = document.getElementById('heading')
-// console.log(Object.keys(heading.style))
-// heading.style.backgroundColor = 'lightblue'
-
-
-//className
 var head = document.getElementById('heading')
-// head.className += ' new-class'
-// console.log(heading.className)
 
-//classList
-heading.classList.toggle('new-class')
-console.log(head.className)
-heading.classList.add('new-class')
-console.log(head.className)
+for (var i = 0; i < 5; i++){
+    var h2 = document.createElement("h2")
+    h2.textContent = 'This is a test message.'
+   h2.style.color = "cornflowerblue"
+   h2.style.fontWeight = "lighter"
+   h2.style.fontFamily = "sans-serif"
+    document.body.append(h2)
+    h2.classList.add("border");
+}
